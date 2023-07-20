2c87007452ea3363d073f3b4a2cf9f8edb7b1a88
Luna <eskw0701@naver.com>
2023-07-20 23:37:18 +0900

    2번 호텔 객실 정보보기 구현

--- Hotel_Reservation/Hotel_Reservation/main.swift
+++ Hotel_Reservation/Hotel_Reservation/main.swift
@@ -5,28 +5,13 @@ class HotelReservation {
     var totalMoney = 0
     var myMoney: [Int] = []
     
-    var hotelList = [
-        "1번방 1박 100000원",
-        "2번방 1박 200000원",
-        "3번방 1박 300000원",
-        "4번방 1박 400000원",
-        "5번방 1박 500000원"
+    var hotelList: [[String:String]] = [
+        ["name":"1번방","date":"1박","price":"100000원","체크인":"00-00", "체크아웃":"00-00"],
+        ["name":"2번방","date":"1박","price":"200000원","체크인":"00-00", "체크아웃":"00-00"],
+        ["name":"3번방","date":"1박","price":"300000원","체크인":"00-00", "체크아웃":"00-00"],
+        ["name":"4번방","date":"1박","price":"400000원","체크인":"00-00", "체크아웃":"00-00"],
+        ["name":"5번방","date":"1박","price":"500000원","체크인":"00-00", "체크아웃":"00-00"],
     ]
-    
-    var list: [[String:String]] = [
-        ["name":"1번방","date":"1박","price":"100000원"],
-        ["name":"2번방","date":"1박","price":"200000원"],
-        ["name":"3번방","date":"1박","price":"300000원"],
-        ["name":"4번방","date":"1박","price":"400000원"]
-    ]
-    
-    let hotel = Hotel(name: "1번방", date: "1박", price: "10000원")
-    let hotel2 = Hotel(name: "1번방", date: "1박", price: "10000원")
-    let hotel3 = Hotel(name: "1번방", date: "1박", price: "10000원")
-    let hotel4 = Hotel(name: "1번방", date: "1박", price: "10000원")
-    let hotel5 = Hotel(name: "1번방", date: "1박", price: "10000원")
-    
-    
     
     func callCenter() {
 
@@ -43,10 +28,7 @@ class HotelReservation {
         print("0. 프로그램 종료하기")
         print("-----------------------------------")
         
-        var calculating = true
-        
-        while calculating {
-            
+        while true {
             print("원하시는 기능의 번호를 입력해 주세요")
             guard let chooseNumStr = readLine(),
                   let num = Int(chooseNumStr) else {
@@ -56,20 +38,14 @@ class HotelReservation {
             
             switch num {
             case 1 :
-                var randomNum = Int.random(in: 100000...500000)
-                // 생성된 램덤 값에 10000을 나누고 곱하면 뒷 자리의 랜덤 숫자가 다 떨어져 나감
-                var randomMoney = (randomNum / 10000) * 10000
-                myMoney.append(randomMoney)
-                totalMoney  += randomMoney
-                print("\(randomMoney)가 입금되었습니다.")
+                randomNum()
             case 2 :
-                print("아시는분.. 아 귀찮아")
+                hotelInfo()
             case 9 :
                 print("통장에 있는 총 금액은 \(totalMoney)원 입니다")
             case 0 :
                 print("종료됩니다")
-                calculating = false
-                continue
+                exit(0)
             default:
                 print("다시 선택해주세요")
                 continue
@@ -77,21 +53,37 @@ class HotelReservation {
             
         } // calculating
         
+        //1번문제
+        func randomNum(){
+            var randomNum = Int.random(in: 100000...500000)
+            // 생성된 램덤 값에 10000을 나누고 곱하면 뒷 자리의 랜덤 숫자가 다 떨어져 나감
+            var randomMoney = (randomNum / 10000) * 10000
+            totalMoney  += randomMoney
+            print("\(randomMoney)가 입금되었습니다.")
+        }
+        
+        //2번문제
+        func hotelInfo(){
+            
+            for option in hotelList {
+                if let name = option["name"],
+                   let date = option["date"],
+                   let price = option["price"] {
+                    print("\(name) \(date) \(price)")
+                }
+                
+            }
+        }
+        
+        //    func showRoom(){
+        //        for i in 1...5 {
+        //            print("\(i)번방 1박 \(i)0000원")
+        //        }
+        //    }
+        
     } // callCenter
     
 } //HotelReservation
-
-class Hotel {
-    let name : String
-    let date : String
-    let price : String
-    
-    init(name: String, date: String, price: String) {
-        self.name = name
-        self.date = date
-        self.price = price
-    }
-}
 
 let call = HotelReservation()
 print(call.callCenter())

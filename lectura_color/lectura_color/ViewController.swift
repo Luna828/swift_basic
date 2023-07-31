74c6ea3d16ded1f0b81f634037395f68eb06e865
Luna <eskw0701@naver.com>
2023-07-31 13:39:37 +0900

    [FEAT]: UIImageView 쓰는 방법

--- lectura_color/lectura_color/ViewController.swift
+++ lectura_color/lectura_color/ViewController.swift
@@ -12,44 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let button = UIButton(type: .system)
-        button.setTitle("Push View Controller", for: .normal)
-        button.addTarget(self, action: #selector(pushViewController), for: .touchUpInside)
-        button.frame = CGRect(x: 200, y: 200, width: 200, height: 100)
-        self.view.addSubview(button)
-        
-        
-        print("viewDidLoad")
-    }
+        let imageview = UIImageView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        let img = UIImage(systemName: "folder.fill")
+        imageview.image = img
+        imageview.contentMode = .scaleAspectFit
+        self.view.addSubview(imageview)
 
-    @objc func pushViewController() {
-        let newViewController = UIViewController()
-        newViewController.title = "New view Controller"
-        newViewController.view.backgroundColor = .black
-        navigationController?.pushViewController(newViewController, animated: true)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("viewWillAppear")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("viewDidAppear")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("viewWillDisappear")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("viewDidDisappear")
-    }
-    
-   
 
 }
 

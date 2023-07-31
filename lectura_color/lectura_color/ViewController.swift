a29f9dfe2f96f9b33d0d7a8034dc5d78c5c2d790
Luna <eskw0701@naver.com>
2023-07-31 14:03:01 +0900

    [FEAT] switch 사용법

--- lectura_color/lectura_color/ViewController.swift
+++ lectura_color/lectura_color/ViewController.swift
@@ -12,12 +12,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let textField = UITextField(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
-        textField.placeholder = "이름을 입력하세요"
-        self.view.addSubview(textField)
-      
+        let btn = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+        btn.setTitle("button", for: .normal)
+        btn.backgroundColor = .blue
+        btn.addTarget(self, action: #selector(btnAction2), for: .touchUpInside)
+        self.view.addSubview(btn)
+        
+        let mySwitch = UISwitch(frame: CGRect(x: 200, y: 200, width: 100, height: 100))
+        mySwitch.isOn = true
+        mySwitch.onTintColor = .green
+        mySwitch.thumbTintColor = .white
+        mySwitch.addTarget(self, action: #selector(switchToggle), for: .valueChanged)
+        self.view.addSubview(mySwitch)
+        
+    }
+    @objc func switchToggle(){
+        print("switch value changed")
     }
     
-
+    
+    @objc func btnAction2(){
+        printContent("push")
+    }
+    
+    
 }
 

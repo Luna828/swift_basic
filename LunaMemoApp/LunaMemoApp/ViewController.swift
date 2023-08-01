d3c34f80f2d1c6988ef9f90e13c7e9aeb753475c
Luna <eskw0701@naver.com>
2023-08-01 11:04:25 +0900

    [FEAT] LunaMemoApp 프로젝트 생성 및 다른 프로젝트 이름 변경

---
+++ LunaMemoApp/LunaMemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LunaMemoApp
+//
+//  Created by t2023-m0050 on 2023/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

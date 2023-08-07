b1539460735a756dff0b7501207421bfae1946b7
Luna <eskw0701@naver.com>
2023-08-07 14:44:28 +0900

    UICollectionViewDelegateFlowLayout 를 extension해서 셀 크기 정해서 빈 간격에 따라서 셀을 배치하기

--- LunaMemoApp/LunaMemoApp/Controller/showMemoCollectionViewController.swift
+++ LunaMemoApp/LunaMemoApp/Controller/showMemoCollectionViewController.swift
@@ -7,17 +7,11 @@ class showMemoCollectionViewController: UIViewController, UICollectionViewDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("안녕")
         //collectionview 설정
         //myCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         myCollectionView.dataSource = self
         myCollectionView.delegate = self
-//        self.myCollectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-//        if let flowlayout = myCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-//        flowlayout.estimatedItemSize = .zero
-//        }
-        myCollectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-     
+        //myCollectionView.contentInset = sectionInsets
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,30 +21,11 @@ class showMemoCollectionViewController: UIViewController, UICollectionViewDelega
         DataManager.shared.fetchMemo()
     }
     
+ //sectionInsets의 공간 만큼 추가되기 때문에 cell이 벌려짐 (전체 패딩) = viewDidLoad()의 myCollectionView.contentInset = sectionInsets 랑 같음
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-     
+
         return sectionInsets
     }
-    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//
-//        let width = collectionView.bounds.width
-//        let height = collectionView.bounds.height
-//        let itemsPerRow: CGFloat = 2
-////        let widthPadding = sectionInsets.left * (itemsPerRow + 1)
-////        let itemsPerColumn: CGFloat = 3
-////        let heightPadding = sectionInsets.top * (itemsPerColumn + 1)
-////        let cellWidth = (width - widthPadding) / itemsPerRow
-////        let cellHeight = (height - heightPadding) / itemsPerColumn
-//
-//        let cellWidth: CGFloat = (width - 20 - itemsPerRow * 2) / 3
-//        let cellHeight: CGFloat = 50
-//        //화면 가로 사이즈 구하기
-//        // cell 3개
-//        // 셀이랑 셀 사이 거리
-//        //가로사이즈 - 셀 사이 거리 * 2 를 빼고 / 3
-//        return CGSize(width: cellWidth, height: cellHeight)
-//    }
     
     // 각 콜렉션뷰 쎌 설정
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -64,33 +39,22 @@ class showMemoCollectionViewController: UIViewController, UICollectionViewDelega
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
         print(DataManager.shared.memoList.count)
-        
         return DataManager.shared.memoList.count
     }
 }
 
+
 extension showMemoCollectionViewController: UICollectionViewDelegateFlowLayout {
     
+    //셀 사이즈를 결정해주는 코드
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let interItemSpacing: CGFloat = 10
-        let padding: CGFloat = 16
-        let width = (collectionView.bounds.width - interItemSpacing * 3 - padding * 2) / 3
-        // print(width)
-        let height = width * 1.2
-    return CGSize(width: width, height: height)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-    return 10
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+        let width = view.bounds.width
+        let itemSpacing: CGFloat = 10 //셀 사이 간격 너비
+        let cellWidth: CGFloat = (width - (sectionInsets.left + sectionInsets.right) - (itemSpacing * 2)) / 3
+        return CGSize(width: cellWidth, height: cellWidth)
     }
 }
-
 
 class memoClass: UICollectionViewCell {
     @IBOutlet weak var collectionBtn: UIButton!
